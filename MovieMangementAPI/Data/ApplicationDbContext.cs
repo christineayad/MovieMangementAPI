dd3837d709d0e46e93827ce47548dcfdf427ba3c
christina ayad <christien.ayed@outlook.com>
2024-12-02 17:32:33 +0200

    Add project files.

---
+++ MovieMangementAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using MovieMangementAPI.Model;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieMangementAPI
+{
+    public class ApplicationDbContext:IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+       public  DbSet<ApplicationUser> ApplicationUsers { get; set; }
+      
+    }
+}
---
+++ MovieMangementAPI/Model/ApplicationUser.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MovieMangementAPI.Model
+{
+    public class ApplicationUser:IdentityUser
+    {
+        public string? FullName { get; set; }
+
+
+        public bool Active { get; set; }
+        public ApplicationUser()
+        {
+
+
+            Active = false;
+        }
+        public string Mobile { get; set; }
+    }
+}
