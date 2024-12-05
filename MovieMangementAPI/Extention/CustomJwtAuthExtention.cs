1580f7a189f1ea63df04f0a84c28b32b26308bfa
christina ayad <christien.ayed@outlook.com>
2024-12-05 14:55:00 +0200

    CustomAuthentication

---
+++ MovieMangementAPI/Extention/CustomJwtAuthExtention.cs
@@ -0,0 +1,77 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+
+namespace TestRESTAPI.Extentions
+{
+    public static class CustomJwtAuthExtention
+    {
+        public static void AddCustomJwtAuth(this IServiceCollection services, ConfigurationManager configuration)
+        {
+            services.AddAuthentication(o =>
+            {
+                o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                o.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                o.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(o =>
+            {
+                o.RequireHttpsMetadata = false;
+                o.SaveToken = true;
+                o.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = true,
+                    ValidIssuer = configuration["JWT:issuer"],
+                    ValidateAudience = false,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Key"]))
+                };
+            });
+        }
+
+        public static void AddSwaggerGenJwtAuth(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(o =>
+            {
+                o.SwaggerDoc("v1", new OpenApiInfo()
+                {
+                    Version = "v1",
+                    Title = "Movie api",
+                    Description = "cinema management api system",
+                    Contact = new OpenApiContact()
+                    {
+                        Name = "Christina ayad",
+                        Email = "christien.ayad@gmail.com",
+                        Url = new Uri("https://mydomain.com")
+                    }
+                });
+
+                o.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "Enter the JWT Key"
+                });
+
+                o.AddSecurityRequirement(new OpenApiSecurityRequirement() {
+                    {
+                       new OpenApiSecurityScheme()
+                       {
+                          Reference = new OpenApiReference()
+                          {
+                             Type = ReferenceType.SecurityScheme,
+                             Id = "Bearer"
+                          },
+                          Name = "Bearer",
+                          In = ParameterLocation.Header
+                       },
+                       new List<string>()
+                    }
+                });
+            });
+        }
+    }
+}
